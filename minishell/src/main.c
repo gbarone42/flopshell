b015c9d31cbffb59261cd51edc95bf4dc9ee2311
gbarone42 <badph3@gmail.com>
2023-09-21 02:04:49 +0200

    21_09

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -9,11 +9,12 @@ int shell_init(t_shell *shell)
     shell->pipe[0] = -1;
     shell->pipe[1] = -1;
     shell->status = 0;
-    user = getenv("USER");
-    shell->prompt = ft_strjoin(user, "@Agshell");
+    user = getenv("USER"); //NOTE : should add a check to ensure that user is not NULL before using it
+    shell->prompt = ft_strjoin(user, "@Agshell"); //Ensure that ft_strjoin correctly allocates memory and returns a valid pointer. 
     // Strjoin does the malloc
     free(user);
     //shell_env(env, shell);
+    //should include a return statement at the end of the function.
 }
 
 void    shell_loop(t_shell *shell, int flag)
@@ -25,7 +26,7 @@ void    shell_loop(t_shell *shell, int flag)
             //signals
             //readline input
             // shell->input = realine(shell->prompt)
-            if ((ft_strcmp(shell->input, "exit", 5) == 0) || shell->input == NULL);
+            if ((ft_strcmp(shell->input, "exit", 5) == 0) || shell->input == NULL)
                 shell_exit(shell);
         }
 }
@@ -36,7 +37,7 @@ int main()
     int     flag;
 
     flag = 0;  
-    shell = init_shell(&shell);
-    init_loop(&shell, &flag);
+    shell = init_shell(&shell);//shell_init o init_shell? testa o croce?
+    init_loop(&shell, &flag);// ⬆️
 
 }
